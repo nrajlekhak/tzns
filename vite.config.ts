fc32a160b8295a3c4e75ab83c8507d3aff3f11ec
Niraj Lekhak <nrajlekhak@gmail.com>
2025-07-05 12:53:34 +0545

    setup tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/tzns/',
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-  preview: {
-    port: 3000,
-  },
+export default defineConfig(async () => {
+  const tailwindcss = await import('@tailwindcss/vite')
+  
+  return {
+    plugins: [react(), tailwindcss.default()],
+    base: '/tzns/',
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+    },
+    server: {
+      port: 3000,
+      open: true,
+    },
+    preview: {
+      port: 3000,
+    },
+  }
 })
